db24084b42de877ed94974602324c6ca94ef06d0
Davion Garcia <dapg.dev@gmail.com>
2025-09-19 05:21:29 -0700

    min min

--- index.js
+++ index.js
@@ -54,17 +54,14 @@ const hobbies = [
   
 ]
 
-// Get the container element from the HTML
 const container = document.getElementById('recommended-container');
 const hobbieContainer = document.getElementById('hobbies-container')
 
 // Loop through the data and create a card for each item
 certs.forEach(item => {
-  // Create the main card div
   const card = document.createElement('div');
   card.className = 'card';
 
-  // Create the card footer to hold the other details
   const cardFooter = document.createElement('div');
   cardFooter.className = 'card-footer';
 
@@ -73,24 +70,18 @@ certs.forEach(item => {
   imgItem.src = item.image;
   card.appendChild(imgItem);
 
-  // Append the card footer to the main card
   card.appendChild(cardFooter);
 
-  // Append the complete card to the container
   container.appendChild(card);
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Get the "Back to Top" button element
   const backToTopButton = document.querySelector('.back-to-top');
 
-  // Add a click event listener to the button
   if (backToTopButton) {
     backToTopButton.addEventListener('click', (e) => {
-      // Prevent the default anchor link behavior
       e.preventDefault();
 
-      // Smoothly scroll to the top of the page
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
@@ -134,16 +125,13 @@ function updateCarousel() {
   const allImages = document.querySelectorAll('.carousel-card');
 
   allImages.forEach((card, index) => {
-    // Select all direct children of the current card
     const cardChildren = card.children;
     console.log(cardChildren)
     if (index === currentIndex) {
-      // If this is the active card, add the 'active' class to its children
       for (const child of cardChildren) {
         child.classList.add('active');
       }
     } else {
-      // If it's not the active card, remove the 'active' class from its children
       for (const child of cardChildren) {
         child.classList.remove('active');
       }
@@ -181,7 +169,6 @@ function updateCarousel() {
     }, 3500);
   }
 
-  // --- Initial setup ---
   createCarouselItems();
   updateCarousel();
   timer()
@@ -189,11 +176,9 @@ function updateCarousel() {
 
 // Hobbies section
 hobbies.forEach(item => {
-  // Create the main card div
   const card = document.createElement('div');
   card.className = 'hobbie-card';
 
-  // Create the card footer to hold the other details
   const cardFooter = document.createElement('div');
 
   const imgItem = document.createElement('img');
@@ -201,8 +186,5 @@ hobbies.forEach(item => {
   imgItem.src = item.image;
   card.appendChild(imgItem);
 
-  // Append the card footer to the main card
-
-  // Append the complete card to the container
   hobbieContainer.appendChild(card);
 });
